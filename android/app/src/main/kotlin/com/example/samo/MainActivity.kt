79901a9e583b54f7c951509ab3065b1eb80e1d10
TrekHub <teddyomondi001@gmail.com>
2020-09-02 13:16:41 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/samo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.samo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:samo/screens/welcome_screen.dart';
+import 'package:samo/screens/login_screen.dart';
+import 'package:samo/screens/registration_screen.dart';
+import 'package:samo/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
